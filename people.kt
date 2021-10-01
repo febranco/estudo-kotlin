5e2b979def30e696547f36618daf68cddd63602f
felipe branco <felipe.branco9@gmail.com>
2021-10-01 20:40:31 -0300

    primeiro commit

---
+++ people.kt
@@ -0,0 +1,23 @@
+package one.digitalInovation.digiobank
+
+class people {
+
+    var name: String = "Felipe"
+    private get
+
+    constructor()
+    fun peopleInfo() = "$nome e $cpf"
+
+
+    var cpf: String = "123.123.123-48"
+     private set
+}
+
+fun main(){
+    val felipe = people()
+
+    println(felipe.peopleInfo())
+    //println(felipe.name)
+    //println(felipe.cpf)
+    //println(felipe.Endereco().rua)
+}
